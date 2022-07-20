7705a786b4c4691ad5c8c36b29e8ffd231259ddd
AtelyPham <tinpham1412000@gmail.com>
2022-07-20 19:25:54 +0700

    feat: add `Transfer` event handler

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,33 +1,22 @@
-import {SubstrateExtrinsic,SubstrateEvent,SubstrateBlock} from "@subql/types";
-import {StarterEntity} from "../types";
-import {Balance} from "@polkadot/types/interfaces";
-
-
-export async function handleBlock(block: SubstrateBlock): Promise<void> {
-    //Create a new starterEntity with ID using block hash
-    let record = new StarterEntity(block.block.header.hash.toString());
-    //Record block number
-    record.field1 = block.block.header.number.toNumber();
-    await record.save();
-}
+import { Balance } from '@polkadot/types/interfaces'
+import { SubstrateEvent } from '@subql/types'
+import { Transfer } from '../types'
 
 export async function handleEvent(event: SubstrateEvent): Promise<void> {
-    const {event: {data: [account, balance]}} = event;
-    //Retrieve the record by its ID
-    const record = await StarterEntity.get(event.block.block.header.hash.toString());
-    record.field2 = account.toString();
-    //Big integer type Balance of a transfer event
-    record.field3 = (balance as Balance).toBigInt();
-    await record.save();
+  const {
+    event: {
+      data: [from, to, amount],
+    },
+  } = event
+
+  // Create the new transfer entity
+  const transfer = new Transfer(
+    `${event.block.block.header.number.toNumber()}-${event.idx}`
+  )
+
+  transfer.blockNumber = event.block.block.header.number.toBigInt()
+  transfer.from = from.toString()
+  transfer.to = to.toString()
+  transfer.amount = (amount as Balance).toBigInt()
+  await transfer.save()
 }
-
-export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
-    const record = await StarterEntity.get(extrinsic.block.block.header.hash.toString());
-    //Date type timestamp
-    record.field4 = extrinsic.block.timestamp;
-    //Boolean tyep
-    record.field5 = true;
-    await record.save();
-}
-
-
